645ca22142c3485da5cf438f90dd83d294b71ed8
nikeshskillup <142885550+nikeshskillup@users.noreply.github.com>
2024-03-18 07:59:52 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,4 @@
-module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true
-  },
-  extends: 'standard',
-  overrides: [
-    {
-      env: {
-        node: true
-      },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
-      parserOptions: {
-        sourceType: 'script'
-      }
-    }
-  ],
-  parserOptions: {
-    ecmaVersion: 8 // Update to support ES8 features
-  },
-  rules: {
-    'no-console': 'off', // Optional: Turn off 'no-console' rule if you want to allow console usage
-    'no-var': 'error', // Optional: Enforce 'const' and 'let' instead of 'var'
-    'prefer-const': 'error' // Optional: Enforce usage of 'const' where possible
-  }
+{
+  ""esversion"": 8,
+  ""moz"": true
 }
