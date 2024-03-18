8c9dfe1ecacc4d8695db93c2d6b612040023432a
nikeshskillup <142885550+nikeshskillup@users.noreply.github.com>
2024-03-18 07:50:01 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,23 @@ module.exports = {
     browser: true,
     commonjs: true,
     es2021: true,
-    node: true
+    node: true // Add node environment
   },
   extends: 'standard',
   overrides: [
     {
-      env: {
-        node: true
-      },
       files: [
-        '.eslintrc.{js,cjs}'
+        '*.js' // Remove '.eslintrc.{js,cjs}' and use '*.js' to match all JavaScript files
       ],
       parserOptions: {
-        ecmaVersion: 6, 
-        sourceType: 'module'
+        ecmaVersion: 6, // Update to use ECMAScript 6
+        sourceType: 'module' // Specify source type as 'module'
       }
     }
   ],
   rules: {
+    'no-var': 'error', // Use 'let' or 'const' instead of 'var'
+    'no-unused-vars': 'warn' // Warn about unused variables
+    // Add more rules as needed
   }
-}
+};
